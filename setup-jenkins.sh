dba0e572ddbbeea077d17cfffb46370f0c4d11e0
tutugodfrey <godfrey_tutu@yahoo.com>
2024-11-21 01:03:05 +0100

    Update setup script

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -3,4 +3,4 @@
 helm repo add jenkins https://charts.jenkins.io
 helm repo update
 kubectl create namespace ci
-helm install --upgrade jenkins jenkins/jenkins --namespace ci --values jenkins.values.yaml
+helm upgrade --install jenkins jenkins/jenkins --namespace ci --values jenkins.values.yaml
